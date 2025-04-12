e620a73312fd12b7bb037797aef0c497438bce82
JOSE CANDIDO <jose.candido_ext@a-to-be.com>
2025-04-12 01:07:31 +0100

    Models

--- tejomag/backend/models.py
+++ tejomag/backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    lead = models.CharField(max_length=200, blank=True, null=True)
+    image_src = models.CharField(max_length=200, blank=True, null=True)
+    link = models.CharField(max_length=200, blank=True, null=True)
+    category = models.CharField(max_length=200, blank=True, null=True)
+    slug = models.CharField(max_length=200, blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    source = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
